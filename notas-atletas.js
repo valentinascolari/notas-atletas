80826ec9f004504c8af8c6c07d439a00521a5692
Valentina <135928276+valentinascolari@users.noreply.github.com>
2025-12-05 22:23:00 -0300

    Add athlete grades

---
+++ notas-atletas.js
@@ -0,0 +1,41 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+for (let i = 0; i < atletas.length; i++) {
+    
+    let atleta = atletas[i];
+
+    let notasParaCalculo = atleta.notas.slice().sort((a, b) => a - b);
+
+    let notasComputadas = notasParaCalculo.slice(1, 4);
+
+    let soma = 0;
+    notasComputadas.forEach(function(nota) {
+        soma = soma + nota;
+    });
+
+    let media = soma / notasComputadas.length;
+
+    let notasParaExibir = atleta.notas.sort(); 
+
+    console.log("Atleta: " + atleta.nome);
+    console.log("Notas Obtidas: " + notasParaExibir.join(","));
+    console.log("Média Válida: " + media);
+    console.log(""); 
+}
